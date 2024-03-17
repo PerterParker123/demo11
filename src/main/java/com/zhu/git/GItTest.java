af96956440eb20db9ca240bba88a94dc27206d42
Zhuzheng <Zhuzheng@qq.com>
2024-03-17 15:58:17 +0800

    demo11 second commit

--- src/main/java/com/zhu/git/GItTest.java
+++ src/main/java/com/zhu/git/GItTest.java
@@ -4,6 +4,7 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world1");
+        System.out.println("hello world2");
 
     }
 }
