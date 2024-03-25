521ee8b611e6d606227c348d5af223ca8aa4a6b4
Zhuzheng <Zhuzheng@qq.com>
2024-03-26 02:40:46 +0800

    push commit3

--- src/main/java/com/zhu/git/GItTest.java
+++ src/main/java/com/zhu/git/GItTest.java
@@ -8,5 +8,6 @@ public class GItTest {
         System.out.println("hello world3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
