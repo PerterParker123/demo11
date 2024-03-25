f74f2f13d4dd5a80128da14fcb425175c1da85b3
Zhuzheng <Zhuzheng@qq.com>
2024-03-26 01:41:00 +0800

    hot-fix commit2

--- src/main/java/com/zhu/git/GItTest.java
+++ src/main/java/com/zhu/git/GItTest.java
@@ -6,5 +6,6 @@ public class GItTest {
         System.out.println("hello world1");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("hot-fix test");
     }
 }
